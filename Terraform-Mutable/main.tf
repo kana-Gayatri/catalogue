dd54b60d7c961611827f64104691bbff13ca168b
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-06 18:16:12 +0530

    test1

--- Terraform-Mutable/main.tf
+++ Terraform-Mutable/main.tf
@@ -1,5 +1,5 @@
 module "cart" {
-  source              ="git::https://github.com/kana-Gayatri/terraform-mutable.git//app-module"
+  source              = "git::https://github.com/kana-Gayatri/terraform-mutable.git//app-module"
   INSTANCE_TYPE       = var.INSTANCE_TYPE
   ENV                 = var.ENV
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
@@ -8,9 +8,11 @@ module "cart" {
   PORT                = 8080
   IS_PRIVATE_LB       = true
   LB_RULE_PRIORITY    = 101
+#  APP_VERSION         = var.APP_VERSION
+#  NEXUS_USER          = var.NEXUS_USER
+#  NEXUS_PASS          = var.NEXUS_PASS
 }
 
 output "INSTANCE_IDS" {
   value = module.cart.INSTANCE_IDS
 }
-
