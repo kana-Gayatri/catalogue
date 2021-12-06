6e59bf944c6928f5537fa47c5d486fa32376509d
Gayatri <gayatrikanaparthi@gmail.com>
2021-12-06 20:00:55 +0530

    test1

--- Terraform-Mutable/main.tf
+++ Terraform-Mutable/main.tf
@@ -5,7 +5,7 @@ module "cart" {
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
 #  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
 #  COMPONENT           = "catalogue"
-#  PORT                = 8080
+  PORT                = 8080
   IS_PRIVATE_LB       = true
   LB_RULE_PRIORITY    = 101
 #  APP_VERSION         = var.APP_VERSION
