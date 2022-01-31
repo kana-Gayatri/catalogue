8bab03b1ba6830afd66f3269041b35b161fd4f0d
Gayatri <gayatrikanaparthi@gmail.com>
2022-01-31 19:16:06 +0530

    folder name changed as terraform-mutable

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -3,5 +3,5 @@ variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "APP_VERSION" {}
-variable "NEXUS_USER" {}
-variable "NEXUS_PASS" {}
+#variable "NEXUS_USER" {}
+#variable "NEXUS_PASS" {}
